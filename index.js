b2107d2ed1a2586d00259ebdcd841ae53e69f7ae
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2023-10-08 19:49:13 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,168 @@
+require("dotenv").config();
+const cors = require("cors");  // react can connect with server with the help of cors.
+const express = require("express");   // to start the server
+const connectDB = require("./connectDB");
+const Book = require('./models/Books');
+const multer = require("multer");     // 
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+// middlewares below
+app.use(cors());
+app.use(express.urlencoded( { extended: true } ));
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));  // to give access to db
+
+// all apis below
+
+
+
+// Get All Books
+app.get("/api/books", async (req, res) => {
+  try {
+    const category = req.query.category;   // filtering data based on filters
+    //const stars = req.query.stars;    // you can also do based on rating, price, title a-z
+
+    const filter = {};
+    if(category) {
+      filter.category = category;
+    }
+
+    const data = await Book.find(filter);
+    
+    if (!data) {
+      throw new Error("An error occurred while fetching books.");
+    }
+    
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching books." });
+  }
+});
+
+// Get A Single Books
+app.get("/api/books/:slug", async (req, res) => {
+  try {
+    const slugParam = req.params.slug;
+    const data = await Book.findOne({ slug: slugParam});
+
+    if (!data) {
+      throw new Error("An error occurred while fetching a book.");
+    }
+    
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching books." });
+  }
+});
+
+
+
+// Create A Book
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/api/books", upload.single("thumbnail")  ,async (req, res) => {
+  try {
+    console.log(req.body);
+    console.log(req.file);
+
+    const newBook = new Book({
+      title: req.body.title,
+      slug: req.body.slug,
+      stars: req.body.stars,
+      description: req.body.description,
+      category: req.body.category,
+      thumbnail: req.file.filename,
+      price:req.body.price
+    })
+
+    await Book.create(newBook);
+    res.json("Data Submitted");
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching books." });
+  }
+});
+
+// Update A Book
+app.put("/api/books", upload.single("thumbnail"), async (req, res) => {
+  try {
+
+    const bookId = req.body.bookId;
+
+    const updateBook = {
+      title: req.body.title,
+      slug: req.body.slug,
+      stars: req.body.stars,
+      description: req.body.description,
+      category: req.body.category,
+      price:req.body.price
+    }
+
+    if (req.file) {
+      updateBook.thumbnail = req.file.filename;
+    }
+
+    await Book.findByIdAndUpdate(bookId, updateBook)
+    res.json("Data Submitted");
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching books." });
+  }
+});
+
+// to delete Book
+app.delete("/api/books/:id", async(req,res) => {
+  const bookId = req.params.id;
+
+  try {
+    await Book.deleteOne({_id: bookId});
+    res.json("How dare you!" + req.body.bookId);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+
+// app.post("/api/books", async (req, res) => {
+//   try {
+//     console.log(req.body);
+
+//     const newBook = new Book({
+//       title: req.body.title,
+//       slug: req.body.slug,
+//       stars: req.body.stars,
+//       description: req.body.description,
+//       category: req.body.category,
+//       //thumbnail: req.file.thumbnail,
+//     })
+
+//     await Book.create(newBook);
+//     res.json("Data Submitted");
+//   } catch (error) {
+//     res.status(500).json({ error: "An error occurred while fetching books." });
+//   }
+// });
+
+app.get("/", (req, res) => {
+  res.json("Hello mate!");
+});
+
+app.get("*", (req, res) => {
+  res.sendStatus("404");
+});
+
+app.listen(PORT, ()=> {
+  console.log(`Server is running on Port: ${PORT}`);
+});
